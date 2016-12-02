1f0c102125c204502c088ff0091445b0d2559271
Julia Paxton <jcpax5@Julias-MacBook-Pro-3.local>
2016-12-02 17:38:18 -0600

    new commit

---
+++ app/data/friends.js
@@ -0,0 +1,147 @@
+[
+  {
+    "name": "Danyelle",
+    "photo": "CSS/Images/Danyelle.jpg",
+    "location": "Austin, TX, USA",
+    "email": "Danyelle@gmail.com",
+    "scores": [
+      "4",
+      "2",
+      "2",
+      "4",
+      "3"
+    ]
+  },
+  {
+    "name": "Courtney",
+    "photo": "CSS/Images/Courtney.jpg",
+    "location": "Austin, TX, USA",
+    "email": "Courtney@gmail.com",
+    "scores": [
+      "2",
+      "1",
+      "4",
+      "2",
+      "5"
+    ]
+  },
+  {
+    "name": "Nathaniel",
+    "photo": "CSS/Images/nathaniel.jpg",
+    "location": "Austin, TX, USA",
+    "email": "Nathaniel@gmail.com",
+    "scores": [
+      "1",
+      "3",
+      "1",
+      "3",
+      "3"
+    ]
+  },
+  {
+    "name": "Blake",
+    "photo": "CSS/Images/Blake.jpg",
+    "location": "Austin, TX, USA",
+    "email": "Blake@gmail.com",
+    "scores": [
+      "5",
+      "3",
+      "5",
+      "5",
+      "5"
+    ]
+  },
+  {
+    "name": "Clark",
+    "photo": "CSS/Images/Clark.jpg",
+    "location": "Austin, TX, USA",
+    "email": "Clark@gmail.com",
+    "scores": [
+      "3",
+      "2",
+      "4",
+      "2",
+      "5"
+    ]
+  },
+  {
+    "location": "78759",
+    "scores": [
+      "1",
+      "3",
+      "1",
+      "3",
+      "2"
+    ]
+  },
+  {
+    "location": "78759",
+    "scores": [
+      "1",
+      "3",
+      "2",
+      "3",
+      "2"
+    ]
+  },
+  {
+    "location": "78759",
+    "scores": [
+      "1",
+      "3",
+      "2",
+      "5",
+      "2"
+    ]
+  },
+  {
+    "location": "78759",
+    "scores": [
+      "3",
+      "3",
+      "2",
+      "5",
+      "2"
+    ]
+  },
+  {
+    "location": "78759",
+    "scores": [
+      "1",
+      "1",
+      "2",
+      "5",
+      "2"
+    ]
+  },
+  {
+    "location": "78759",
+    "scores": [
+      "1",
+      "1",
+      "2",
+      "5",
+      "5"
+    ]
+  },
+  {
+    "location": "78759",
+    "scores": [
+      "1",
+      "1",
+      "5",
+      "5",
+      "5"
+    ]
+  },
+  {
+    "location": "denver",
+    "scores": [
+      "1",
+      "1",
+      "5",
+      "5",
+      "5"
+    ]
+  }
+]
